64950727d72174ab7c1d0c35193bfa3261c2f7ee
Sergei <gorodilov.cad@gmail.com>
2022-12-25 19:06:52 +0300

    Added Program folder, solution found. Release

---
+++ Program/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int arrayCapacity = 10;
+string[] originalArray = new string[arrayCapacity];
+string[] matchedArray = new string[originalArray.Length];
+int matches = 0;
+int lessOrEquial = 3;
+string userInput = string.Empty;
+
+for (int i = 0; i < originalArray.Length; i++)
+{
+    userInput = string.Empty;
+    while (userInput == string.Empty)
+    {
+        System.Console.WriteLine($"Input string number {i}:");
+        userInput = Console.ReadLine()!;
+    }
+
+    originalArray[i] = userInput;
+
+    if (originalArray[i].Length <= lessOrEquial)
+    {
+        matchedArray[matches] = originalArray[i];
+        matches++;
+    }
+}
+
+Array.Resize(ref matchedArray, matches);
+
+for (int i = 0; i < matchedArray.Length; i++)
+{
+    System.Console.WriteLine($"Matched element {i} is {matchedArray[i]}");
+}
